d5a4c3114483b15bafaa5f87d2c7d6a1c97dd46f
dhanyasrigrandhi <2100030130@kluniversity.in>
2023-12-07 10:38:24 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-
+import React from 'react';
 import './App.css';
 import Header from './Header';
 import Products from './Products';
+
 function App() {
+  function doSomething(data) {
+    console.log("hello " + data);
+  }
+
   return (
     <div className="App">
-      <Header/>
-      <Products/>
+      <Header />
+      <Products />
+      <button className='btn' onClick={() => {
+        doSomething("welcome");
+      }}>Click</button>
     </div>
   );
 }
